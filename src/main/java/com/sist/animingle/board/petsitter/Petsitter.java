a23b6173c4f14ce17d6b5dcbb91ab8ccf0ad3a02
leeeeeeeminji <ldfu0269@naver.com>
2023-06-02 14:28:34 +0900

    2. 펫시터 1차 커밋 (미완성)

---
+++ src/main/java/com/sist/animingle/board/petsitter/Petsitter.java
@@ -0,0 +1,23 @@
+package com.sist.animingle.board.petsitter;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/board/petsitter.do")
+public class Petsitter extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/board/petsitter/petsitter.jsp");
+		dispatcher.forward(req, resp);
+
+	}
+
+}
