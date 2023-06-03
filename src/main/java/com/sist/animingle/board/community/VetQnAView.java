1e4cd3065639ab2d5e88605071124329cd8e09c6
Standard-B <wjdtjr030@naver.com>
2023-06-03 21:14:56 +0900

    수의사 QnA 상세보기 페이지 추가

---
+++ src/main/java/com/sist/animingle/board/community/VetQnAView.java
@@ -0,0 +1,23 @@
+package com.sist.animingle.board.community;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/board/vetqnaview.do")
+public class VetQnAView extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/board/community/vetqnaview.jsp");
+		dispatcher.forward(req, resp);
+
+	}
+
+}
